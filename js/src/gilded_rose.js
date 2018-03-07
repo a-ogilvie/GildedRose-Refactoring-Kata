0238f71834cb5cb9f4a73ab53fca357db33d3f8f
Alexander Ogilvie <alexander148@tea.seibubunri.jp>
2018-03-07 10:36:40 +0900

    Rewrote updateQuality to use forEach

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -7,55 +7,70 @@ class Item {
 }
 
 class Shop {
-  constructor(items=[]){
+  constructor(items=[]) {
     this.items = items;
   }
+  
   updateQuality() {
-    for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
+    this.items.forEach(item => {
+      //if (item.name === 'normal') return;
+        // reduce quality for normal items
+        if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+          if (item.quality > 0) {
+            if (item.name != 'Sulfuras, Hand of Ragnaros') {
+              item.quality = item.quality - 1;
             }
           }
         }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
+        
+        // increase quality for special items
+        else {
+          // the quality cannot be more than 50
+          if (item.quality < 50) {
+            item.quality = item.quality + 1;
+            if (item.name == 'Backstage passes to a TAFKAL80ETC concert')
+            // logic to increase quality of backstage pass depending on time remaining
+            {
+              if (item.sellIn < 11) {
+                if (item.quality < 50) {
+                  item.quality = item.quality + 1;
+                }
               }
+              if (item.sellIn < 6) {
+                if (item.quality < 50) {
+                  item.quality = item.quality + 1;
+                }
+              }
+            }
+          }
+        }
+        
+        // decrease sell by date for all items except sulfuras
+        if (item.name != 'Sulfuras, Hand of Ragnaros') {
+          item.sellIn = item.sellIn - 1;
+        }
+       
+        if (item.sellIn < 0) {
+          if (item.name != 'Aged Brie') {
+            if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+              // decrease quality of regular items
+              if (item.quality > 0) {
+                if (item.name != 'Sulfuras, Hand of Ragnaros') {
+                  item.quality = item.quality - 1;
+                }
+              }
+            } 
+             // backstage passes 
+            else {
+              item.quality = item.quality - item.quality;
             }
           } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
+            if (item.quality < 50) {
+              item.quality = item.quality + 1;
+            }
           }
         }
-      }
-    }
+    });
 
     return this.items;
   }
