e130c691179a87cede48d4b3ad39b2dd3f765a02
Alexander Ogilvie <alexander148@tea.seibubunri.jp>
2018-03-07 12:09:08 +0900

    Deleted HUGE conditional

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -23,67 +23,10 @@ class Shop {
         case 'Backstage passes to a TAFKAL80ETC concert':
           updateBackstage(item);
           return;
-        case 'normal':
+        default:
           updateNormal(item);
           return;
       }
-
-      // reduce quality for normal items
-        if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (item.quality > 0) {
-            if (item.name != 'Sulfuras, Hand of Ragnaros') {
-              item.quality = item.quality - 1;
-            }
-          }
-        }
-        
-        // increase quality for special items
-        else {
-          // the quality cannot be more than 50
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-            if (item.name == 'Backstage passes to a TAFKAL80ETC concert')
-            // logic to increase quality of backstage pass depending on time remaining
-            {
-              if (item.sellIn < 11) {
-                if (item.quality < 50) {
-                  item.quality = item.quality + 1;
-                }
-              }
-              if (item.sellIn < 6) {
-                if (item.quality < 50) {
-                  item.quality = item.quality + 1;
-                }
-              }
-            }
-          }
-        }
-        
-        // decrease sell by date for all items except sulfuras
-        if (item.name != 'Sulfuras, Hand of Ragnaros') {
-          item.sellIn = item.sellIn - 1;
-        }
-       
-        if (item.sellIn < 0) {
-          if (item.name != 'Aged Brie') {
-            if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-              // decrease quality of regular items
-              if (item.quality > 0) {
-                if (item.name != 'Sulfuras, Hand of Ragnaros') {
-                  item.quality = item.quality - 1;
-                }
-              }
-            } 
-             // backstage passes 
-            else {
-              item.quality = item.quality - item.quality;
-            }
-          } else {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1;
-            }
-          }
-        }
     });
 
     return this.items;
@@ -130,5 +73,4 @@ function updateBackstage(item) {
   if (item.sellIn < 5) {
     item.quality++;
   }
-
 }
